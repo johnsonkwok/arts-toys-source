e4ed6d032b2baecdda60684518907086ff5e924a
Johnson Kwok <johnsonkwok1@gmail.com>
2019-01-26 15:12:50 -0800

    Add files for components

---
+++ client/src/components/Toolbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Search from './Search.jsx';
+
+const Toolbar = () => (
+    <div>
+      <div>My Collection</div>
+      <div>Wishlist</div>
+      <div><Search /></div>
+    </div>
+);
+
+export default Toolbar;
