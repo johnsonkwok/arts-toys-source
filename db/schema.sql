6a8d0892b92879bde6f17ec59f0ecb8d801baf59
Johnson Kwok <johnsonkwok1@gmail.com>
2019-01-28 11:32:10 -0800

    Update schema to seed db with data from csv file

--- db/schema.sql
+++ db/schema.sql
@@ -4,9 +4,11 @@ CREATE DATABASE IF NOT EXISTS toys_db;
 
 USE toys_db;
 
+DROP TABLE IF EXISTS toys;
+
 CREATE TABLE IF NOT EXISTS toys (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(100) NOT NULL,
+  name VARCHAR(100) NOT NULL UNIQUE,
   retail_price INT,
   est_value INT,
   company VARCHAR(50) NOT NULL,
@@ -19,3 +21,10 @@ CREATE TABLE IF NOT EXISTS toys (
   release_year INT,
   tags VARCHAR(300)
 );
+
+LOAD DATA LOCAL INFILE '/Users/johnsonkwok/Documents/HR/Week-10/hrsf107-mvp/db/toys_data.csv' 
+  INTO TABLE toys
+  FIELDS TERMINATED BY ',' 
+  LINES TERMINATED BY '\n'
+  IGNORE 1 ROWS
+  (name,retail_price,est_value,company,type,category,item_num,property,exclusive_to,picture,release_year,tags);
